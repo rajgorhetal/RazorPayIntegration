6967044f32e7e1b65db312c64f05e4f38b37127b
Hetal Rajgor <hetalrajgorb@gmail.com>
2022-04-12 23:34:56 +0530

    adding UI related changes for razorpay

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 const express = require("express");
 const Razorpay = require("razorpay");
 const app = express();
+app.use(express.static("./public"));
+app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hola</h1>");
-});
+// app.get("/", (req, res) => {
+//   res.send("<h1>Hola</h1>");
+// });
 
 app.post("/order", async (req, res) => {
   const amount = req.body.amount;
 
   var instance = new Razorpay({
-    key_id: "rzp_test_MQw8oQYkRvR2Yk",
-    key_secret: "wrjX1oW79Gu5kFsoFIL3ahGw",
+    key_id: "rzp_test_BFq32QMt1ugErN",
+    key_secret: "1Z9nD7yPcbFUfp8rXuneBgSH",
   });
 
   var options = {
     amount: amount * 100, //* 100 because currency comes in paise/lowest value
     currency: "INR",
-    receipt: "receipt#1",
-    notes: {
-      key1: "value3",
-      key2: "value2",
-    },
+    receipt: "receipt#1"
   };
 
   const myOrder = await instance.orders.create(options);
@@ -29,7 +27,7 @@ app.post("/order", async (req, res) => {
   res.status(200).json({
     success: true,
     amount,
-    myOrder,
+    order: myOrder,
   });
 });
 
